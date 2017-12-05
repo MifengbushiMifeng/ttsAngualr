7a0d7e845855b053ea04393ad8c654a704ca17b4
Jonathan Zhou <zhouronghui30@163.com>
2017-12-05 13:17:14 +0800

    extract routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms'
 import {HttpModule} from '@angular/http'
+import {RouterModule} from '@angular/router'
 
 
 import {AppComponent} from './app.component';
 import {HelloWorldComponent} from './hello-world/hello-world.component';
 import {LoginComponent} from './login/login.component';
 import {AuthService} from "./core/auth.service";
-
+import {routing} from './app.routes'
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import {AuthService} from "./core/auth.service";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [
 
