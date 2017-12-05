ddbd755fb9ff986bd4952b8288dd595dbd35ae2a
Jonathan Zhou <zhouronghui30@163.com>
2017-12-05 13:24:14 +0800

    initial of to do list component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms'
-import {HttpModule} from '@angular/http'
-import {RouterModule} from '@angular/router'
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
 
-
-import {AppComponent} from './app.component';
-import {HelloWorldComponent} from './hello-world/hello-world.component';
-import {LoginComponent} from './login/login.component';
+import {AppComponent} from "./app.component";
+import {HelloWorldComponent} from "./hello-world/hello-world.component";
+import {LoginComponent} from "./login/login.component";
 import {AuthService} from "./core/auth.service";
-import {routing} from './app.routes'
+import {routing} from "./app.routes";
+import {TodoComponent} from "./todo/todo.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorldComponent,
-    LoginComponent
+    LoginComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,19 @@
+/**
+ * To do component.
+ * List the to do stuffs and the related status.
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
